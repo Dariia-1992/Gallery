1d7427ce7c5c73021a928a080581c9dd1c64263f
Dariia <pers4040@gmail.com>
2019-06-04 17:10:28 +0300

    AuthenticationDialog

---
+++ app/src/main/java/com/example/gallery/AuthenticationDialog.java
@@ -0,0 +1,13 @@
+package com.example.gallery;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class AuthenticationDialog extends Dialog {
+
+    public AuthenticationDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+}
